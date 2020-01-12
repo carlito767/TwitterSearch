6ee51fa1ae6044af4148da24dce09df6412b24ad
carlito <charles.longuemart@gmail.com>
2020-01-12 18:07:36 +0100

    Search

--- search.py
+++ search.py
@@ -14,18 +14,31 @@ key_ascii = '{}:{}'.format(API_KEY, API_SECRET_KEY).encode('ascii')
 key_base64 = base64.b64encode(key_ascii)
 key_base64 = key_base64.decode('ascii')
 
-# Request
+# Authentication
 base_url = 'https://api.twitter.com/'
-auth_url = '{}oauth2/token'.format(base_url)
 
 auth_headers = {
-  'Authorization': 'Basic {}'.format(key_base64),
-  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    'Authorization': 'Basic {}'.format(key_base64),
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
 }
-
 auth_data = {
-  'grant_type': 'client_credentials'
+    'grant_type': 'client_credentials'
 }
-
+auth_url = '{}oauth2/token'.format(base_url)
 auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
-print(auth_resp.status_code)
+print(f"Authentication status: {auth_resp.status_code}")
+
+# Search
+access_token = auth_resp.json()['access_token']
+
+search_headers = {
+    'Authorization': 'Bearer {}'.format(access_token)    
+}
+search_params = {
+    'q': 'NASA',
+    'result_type': 'recent',
+    'count': 2
+}
+search_url = '{}1.1/search/tweets.json'.format(base_url)
+search_resp = requests.get(search_url, headers=search_headers, params=search_params)
+print(f"Search status: {search_resp.status_code}")
