4a54828e71368ae80f2be396fa646c1b9c97f91e
carlito <charles.longuemart@gmail.com>
2020-01-12 18:31:22 +0100

    JSON file / Tweet in default browser

--- search.py
+++ search.py
@@ -2,14 +2,15 @@
 # https://blog.goodaudience.com/using-the-twitter-api-with-python-c6e8da96d273
 
 import base64
+import json
 import requests
+import webbrowser
 
 # Consumer API keys
 # TODO: use .env file: https://pypi.org/project/python-dotenv/
 API_KEY = 'RV8lBpxB7hiezPVK2HwM6KNPy'
 API_SECRET_KEY = 'gKaAsAP1BUWikumjpJ4MZM0JeLIjgwBqBTkfNYuIgUzPFkNZGM'
 
-# Formatting
 key_ascii = '{}:{}'.format(API_KEY, API_SECRET_KEY).encode('ascii')
 key_base64 = base64.b64encode(key_ascii)
 key_base64 = key_base64.decode('ascii')
@@ -32,13 +33,27 @@ print(f"Authentication status: {auth_resp.status_code}")
 access_token = auth_resp.json()['access_token']
 
 search_headers = {
-    'Authorization': 'Bearer {}'.format(access_token)    
+    'Authorization': 'Bearer {}'.format(access_token)
 }
 search_params = {
     'q': 'NASA',
     'result_type': 'recent',
-    'count': 2
+    'count': 1
 }
 search_url = '{}1.1/search/tweets.json'.format(base_url)
 search_resp = requests.get(search_url, headers=search_headers, params=search_params)
 print(f"Search status: {search_resp.status_code}")
+
+data = json.loads(search_resp.content)
+tweet_id = data['statuses'][0]['id']
+print(f"Tweet Id: {tweet_id}")
+
+# JSON file
+# TODO: use unique filename
+file = open("out.json", "w+")
+json.dump(data, file)
+file.close()
+
+# Tweet in default browser
+tweet_url = f"twitter.com/user/status/{tweet_id}"
+webbrowser.open_new(tweet_url)
