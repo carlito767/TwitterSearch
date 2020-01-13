838a42c8420ceb19aaa08e58495c408f635404e3
carlito <charles.longuemart@gmail.com>
2020-01-13 12:57:36 +0100

    Flask WTF

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates')
+from flask import Flask, redirect, render_template, request, url_for
+from forms import SearchForm
 
-@app.route('/')
-def home():
-    """Landing page."""
-    return render_template('/index.html', title='Lame Site')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = SearchForm()
+    if form.validate_on_submit():
+        return redirect(url_for('search'))
+    return render_template('index.html', title='Twitter Search', form=form)
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    query = request.form.get('query')
+    return f'Request : {query}'
 
 if __name__ == '__main__':
     app.run()
