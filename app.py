228fb7ad4de5170c814c4661e475adcabc2aae4c
carlito <charles.longuemart@gmail.com>
2020-01-14 15:59:41 +0100

    Persistent Search URL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, Response, send_from_directory, url_for
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
 from forms import SearchForm
 import json
 import os
@@ -12,27 +12,31 @@ app.config['SECRET_KEY'] = 'you-will-never-guess'
 def index():
     form = SearchForm()
     if form.validate_on_submit():
-        return redirect(url_for('search'))
+        query = request.form.get('query')
+        count = request.form.get('count')
+
+        content = twitter.search(query, count)
+
+        # JSON file
+        f = tempfile.NamedTemporaryFile(prefix='search_', suffix='.json', mode='w+', delete=False)
+        print(f'JSON path: {f.name}')
+        filename = os.path.basename(f.name)
+        print(f'JSON filename: {filename}')
+        json.dump(content, f)
+        f.close()
+
+        return redirect(url_for('search', filename=filename))
     return render_template('index.html', title='Twitter Search', form=form)
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    query = request.form.get('query')
-    count = request.form.get('count')
-
-    content = twitter.search(query, count)
-
-    # JSON file
-    f = tempfile.NamedTemporaryFile(prefix='search_', suffix='.json', mode='w+', delete=False)
-    print(f'JSON path: {f.name}')
-    filename = os.path.basename(f.name)
-    print(f'JSON filename: {filename}')
-    json.dump(content, f)
-    f.close
+@app.route('/search/<filename>')
+def search(filename):
+    f = open(os.path.join(tempfile.gettempdir(), filename), "r")
+    content = json.load(f)
+    f.close()
 
     return render_template('search.html', title='Twitter Search', content=content, filename=filename)
 
-@app.route('/download/<path:filename>', methods=['GET', 'POST'])
+@app.route('/download/<filename>', methods=['GET', 'POST'])
 def download(filename):
     print(f'Download JSON file: {filename}')
     return send_from_directory(tempfile.gettempdir(), filename, as_attachment=True)
