40da7ac4dbee8993ef7e175d970a349cbd8c954e
carlito <charles.longuemart@gmail.com>
2020-01-16 13:43:46 +0100

    Handle Form errors

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, IntegerField, SelectField, StringField, SubmitField
 from wtforms.fields.html5 import DateField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Length, Optional
 
 class SearchForm(FlaskForm):
-    query = StringField('Query', validators=[DataRequired()], render_kw={'autofocus': True})
+    query = StringField('Query', validators=[DataRequired(), Length(min=3)], render_kw={'autofocus': True})
     no_retweets = BooleanField('No Retweets', default=True)
     n = IntegerField('Number of tweets', validators=[DataRequired()], default=15)
     result_type = SelectField('Type of results', choices=[('mixed', 'Mixed'), ('recent', 'Recent'), ('popular', 'Popular')], default='recent')
