de48a807b723da4a142b3a9a8580c60aa605ac31
carlito <charles.longuemart@gmail.com>
2020-01-25 18:34:46 +0100

    Remove empty lines in Jinja2 generated output

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+# Remove empty lines in Jinja2 generated output
+app.jinja_env.lstrip_blocks = True
+app.jinja_env.trim_blocks = True
 
 if app.config['ENV'] == 'production':
     app.config.from_object('config.ProductionConfig')
